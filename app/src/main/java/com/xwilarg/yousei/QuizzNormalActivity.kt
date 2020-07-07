c45b1e9a3d86234aa336f3627ee9d05bdb2c5024
Xwilarg <xwilarg@yahoo.fr>
2020-07-07 15:26:22 +0200

    Add quizz view

--- app/src/main/java/com/xwilarg/yousei/QuizzNormalActivity.kt
+++ app/src/main/java/com/xwilarg/yousei/QuizzNormalActivity.kt
@@ -1,11 +1,21 @@
 package com.xwilarg.yousei
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class QuizzNormalActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quizz_normal)
+    }
+
+    fun answer(view: View) {
+        loadQuestion()
+    }
+
+    fun loadQuestion() {
+
     }
 }
